a7dd696767fc11966b2f5d97b8169ece4b3dec25
phamtrungkien0506 <phamtrungkien0506@gmail.com>
2023-12-22 01:06:24 +0700

    ⚡ Cũng không có gì

--- BOT_TRACKING_BINGX.py
+++ BOT_TRACKING_BINGX.py
@@ -1,39 +1,58 @@
 import sys
 import json
+import pandas as pd
 
 from time import sleep
 from core.engine.bingx import BingXEngine
 
 from utils.logger import print_log, save_json_locally
 
+
 class BingXTrackingBot:
-    def __init__(self, api_key, api_secret):
+    def __init__(self, api_key, api_secret, path):
         self.client = BingXEngine(api_key, api_secret, True)
-        self.tracking_data = {}
+        self.params = pd.read_excel(path)
+        try:
+            with open('data/tracking_data.json', 'r') as f:
+                self.tracking_data = json.load(f)
+        except:
+            self.tracking_data = {}
 
     def check_roe(self, position):
         symbol = position['symbol']
         position_id = position['positionId']
         position_side = position['positionSide']
         
-        side_weight = { 'LONG': 1, 'SHORT': -1 }[position_side]
-        close_side = { 1: 'SELL', -1: 'BUY' }[side_weight]
+        if position_side == 'LONG':
+            side_weight = 1
+            dca_side = 'BUY'
+            close_side = 'SELL'
+        elif position_side == 'SHORT':
+            side_weight = -1
+            dca_side = 'SELL'
+            close_side = 'BUY'
 
         leverage = float(position['leverage'])
-        quantity = float(position['positionAmt']) * side_weight
-        mark_price = float(position['markPrice'])
-        entry_price = float(position['entryPrice'])
+        quantity = float(position['positionAmt'])
+        # mark_price = float(position['markPrice'])
+        entry_price = float(position['avgPrice'])
 
-        lastest_price = self.client.latest_price(symbol)
+        try:
+            lastest_price = float(self.client.latest_price(symbol)['price'])
+        except Exception as e:
+            print_log(f"Error getting latest price of {symbol}: {e}")
 
-        roe_v1  = side_weight * (mark_price / entry_price - 1) * leverage * 100
+        # roe_v1  = side_weight * (mark_price / entry_price - 1) * leverage * 100
         roe_v2  = side_weight * (lastest_price / entry_price - 1) * leverage * 100
         
-        min_roe = min(roe_v1, roe_v2)
-        max_roe = max(roe_v1, roe_v2)
+        # min_roe = min(roe_v1, roe_v2)
+        # max_roe = max(roe_v1, roe_v2)
 
-        for i, row in PARAMS.itterrows():
-            if self.tracking_data[position_id]['trackingData'][i]: continue
+        min_roe = roe_v2
+        max_roe = roe_v2
+
+        for i, row in self.params.iterrows():
+            if i in self.tracking_data[position_id]['trackingData']: continue
 
             roe = row['ROE']
             value = row['Value']
@@ -64,9 +83,8 @@ class BingXTrackingBot:
                     'type': 'STOP_MARKET' if roe < 0 else 'TAKE_PROFIT_MARKET',
                     'side': close_side,
                     'positionSide': position_side,
-                    'price': (-side_weight * roe / leverage / 100 + 1) * entry_price,
                     'quantity': new_quantity,
-                    'stopPrice': None,
+                    'stopPrice': (-side_weight * roe / leverage / 100 + 1) * entry_price,
                 },
                 'CLOSE': {
                     'symbol': symbol,
@@ -78,15 +96,26 @@ class BingXTrackingBot:
                 'DCA': {
                     'symbol': symbol,
                     'type': 'MARKET',
-                    'side': close_side,
+                    'side': dca_side,
                     'positionSide': position_side,
                     'quantity': new_quantity,
                 }
             }
 
+            try:
+                params = order_params[order_type]
+                order = self.client.futures_create_order_freestyle(params)['order']
+                print(order)
+                print_log(f"{i}. orderId={order['orderId']}, quantity={new_quantity}")
+                self.tracking_data[position_id]['trackingData'][i] = order['orderId']
+            except Exception as e:
+                print(i, params)
+                print_log(f"Error ordering open order: {e}")
+                raise
+
     def interval_fn(self):
         try:
-            list_open_position = self.client.positions(symbol='')
+            list_open_position = self.client.futures_position_information(symbol='')
         except Exception as e:
             print_log(f"Error getting open positions: {e}")
             return
@@ -95,30 +124,36 @@ class BingXTrackingBot:
         list_curr_position_id = [p['positionId'] for p in list_open_position]
 
         list_closed_position_id = list(set(list_prev_position_id) - set(list_curr_position_id))
-        list_latest_position_id = list(set(list_curr_position_id) - set(list_prev_position_id))
+        list_opened_position_id = list(set(list_curr_position_id) - set(list_prev_position_id))
 
-        for position_id in list_latest_position_id:
+        for position_id in list_opened_position_id:
+            print(f"Opened: {position_id}")
             pos = [p for p in list_open_position if p['positionId'] == position_id][0]
             self.tracking_data[pos['positionId']] = pos
             self.tracking_data[pos['positionId']]['trackingData'] = {}
 
         for position_id in list_closed_position_id:
+            print(f"Closed: {position_id}")
             symbol = self.tracking_data[position_id]['symbol']
             try:
                 self.client.cancel_all_orders(symbol)
+                del self.tracking_data[position_id]
             except Exception as e:
                 print_log(f"Error cancelling open orders: {e}")
 
         for pos in list_open_position: self.check_roe(pos)
 
     def run_until_disconnected(self):
+        counter = 0
         while True:
             try:
                 self.interval_fn()
+                counter += 1
+                if counter % 20 == 0:
+                    save_json_locally('data/tracking_data.json', self.tracking_data)
                 sleep(2)
             except KeyboardInterrupt:
                 sys.exit()
             except:
                 raise
     
-
